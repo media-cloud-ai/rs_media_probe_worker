7ce7bfa0b39c10b668d3903d3ffc074f4de49c24
Valentin NOEL <valentin.noel@media-io.com>
2020-04-29 17:41:14 +0200

    Process simple probe and return result as JSON

--- src/message.rs
+++ src/message.rs
@@ -1,12 +1,36 @@
+use log::LevelFilter;
 use mcai_worker_sdk::job::{Job, JobResult, JobStatus};
-use mcai_worker_sdk::{Channel, MessageError};
+use mcai_worker_sdk::{Channel, MessageError, ParametersContainer};
+use stainless_ffmpeg::probe::Probe;
 
 pub const SOURCE_PATH_PARAMETER: &'static str = "source_path";
 
 pub fn process(
   _channel: Option<&Channel>,
-  _job: &Job,
+  job: &Job,
   job_result: JobResult,
 ) -> Result<JobResult, MessageError> {
-  Ok(job_result.with_status(JobStatus::Completed))
+  let source_path = job
+    .get_string_parameter(SOURCE_PATH_PARAMETER)
+    .ok_or_else(|| {
+      MessageError::ProcessingError(
+        job_result
+          .clone()
+          .with_status(JobStatus::Error)
+          .with_message(&format!(
+            "Invalid job message: missing expected '{}' parameter.",
+            SOURCE_PATH_PARAMETER
+          )),
+      )
+    })?;
+
+  let mut probe = Probe::new(&source_path);
+  probe.process(LevelFilter::Off).unwrap();
+  let result = serde_json::to_string(&probe).unwrap();
+
+  Ok(
+    job_result
+      .with_status(JobStatus::Completed)
+      .with_message(&result),
+  )
 }
